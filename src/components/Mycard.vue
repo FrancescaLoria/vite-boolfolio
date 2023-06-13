049e22311196dd9539cf8ce318fb50ce3b05aa0a
FrancescaLoria <francesca.loria90@hotmail.it>
2023-06-13 17:36:59 +0200

    add my card

---
+++ src/components/Mycard.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "Mycard",
+  props: {
+    project: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <h4 class="title-card">TITOLO: {{ project.title }}</h4>
+    <p class="content-card my-2">CONTENUTO: {{ project.content }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
