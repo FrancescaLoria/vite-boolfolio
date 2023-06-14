fce92f229977e0dc477705988594a448d0cea87b
FrancescaLoria <francesca.loria90@hotmail.it>
2023-06-14 15:37:49 +0200

    function "pwContent" in computed

--- src/components/Mycard.vue
+++ src/components/Mycard.vue
@@ -4,13 +4,24 @@ export default {
   props: {
     project: Object,
   },
+  computed: {
+    pwContent() {
+      if (!this.project.content) {
+        return "Nessun contenuto aggiunto";
+      } else if (this.project.content.length > 150) {
+        return this.project.content.substring(0, 150) + "...";
+      } else {
+        return this.project.content;
+      }
+    },
+  },
 };
 </script>
 
 <template>
   <div class="card p-3">
     <h4 class="title-card">TITOLO: {{ project.title }}</h4>
-    <p class="content-card my-2">CONTENUTO: {{ project.content }}</p>
+    <p class="content-card my-2">CONTENUTO: {{ pwContent }}</p>
   </div>
 </template>
 
